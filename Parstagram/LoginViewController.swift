04226203cea07271b80c4b6f499a1057b274b881
Lily Pham <lilypham@lilys-macbook-pro.local>
2019-02-14 19:39:27 -0800

    Created project with cocoa pods

---
+++ Parstagram/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  Parstagram
+//
+//  Created by Lily Pham on 2/14/19.
+//  Copyright © 2019 Lily Pham. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
