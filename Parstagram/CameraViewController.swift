0489b95de02a817780979c55447883821a32c888
[lilphunky] <[phamlily99@gmail.com]>
2019-02-20 15:28:56 -0800

    Saves post object to parse

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AlamofireImage
+import Parse
 
 class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -45,6 +46,23 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     }
     
     @IBAction func onSubmitButton(_ sender: Any) {
+        let post = PFObject(className: "Posts")
+        
+        post["caption"] = captionTextField.text
+        post["author"] = PFUser.current()
+        
+        let imageData = imageView.image!.pngData()
+        let file = PFFileObject(data: imageData!)
+        post["image"] = file
+        
+        post.saveInBackground { (success, error) in
+            if success {
+                self.dismiss(animated: true, completion: nil)
+                print("Saved!")
+            } else {
+                print("Error saving post")
+            }
+        }
     }
     
 }
