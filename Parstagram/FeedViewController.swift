f37b006b93c23437d27b06c57175bb926a4cc95f
[lilphunky] <[phamlily99@gmail.com]>
2019-02-20 20:48:14 -0800

    Feed fetches the image; allow un-secure urls in info.plist

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -48,6 +48,10 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         cell.usernameLabel.text = user.username
         cell.captionLabel.text = post["caption"] as! String
         
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        cell.photoView.af_setImage(withURL: url)
         
         return cell
     }
