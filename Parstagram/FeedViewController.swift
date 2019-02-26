7d4249d704fab7679f8614ac13082c20b967cd78
[lilphunky] <[phamlily99@gmail.com]>
2019-02-26 12:11:41 -0800

    Save comment from message input bar

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -18,6 +18,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     var showCommentBar = false
     
     var posts = [PFObject]()
+    var selectedPost: PFObject!
     var numberOfPosts: Int!
     
     let myRefreshControl = UIRefreshControl()
@@ -77,6 +78,21 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func messageInputBar(_ inputBar: MessageInputBar, didPressSendButtonWith text: String) {
         // Create the comment
+        let comment = PFObject(className: "Comment")
+        comment["text"] = text
+        comment["post"] = selectedPost
+        comment["author"] = PFUser.current()
+        
+        selectedPost.add(comment, forKey: "comments")
+        
+        selectedPost.saveInBackground { (success, error) in
+            if success {
+                print("Comment saved!")
+            } else {
+                print("Error saving comment.")
+            }
+        }
+        tableView.reloadData()
         
         // Clear and dismiss the input bar
         commentBar.inputTextView.text = nil
@@ -162,6 +178,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
             showCommentBar = true
             becomeFirstResponder()
             commentBar.inputTextView.becomeFirstResponder()
+            selectedPost = post
         }
     }
     
